5efd9aae11f6dc63c25d4eea17feecbe99fafeeb
Mayur <99334268+devopsmayur@users.noreply.github.com>
2023-07-03 14:50:43 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -10,3 +10,24 @@ variable "db_password" {
   description = "RDS root user password"
   sensitive   = true
 }
+
+variable "prefix" {
+  type        = string
+  description = "This prefix will be included in the name of most resources."
+}
+
+variable "region" {
+  type        = string
+  description = "The region where the resources are created."
+}
+
+variable "env" {
+  type        = string
+  description = "Value for the environment tag."
+}
+
+variable "department" {
+  type        = string
+  description = "Value for the department tag."
+  default     = "DBA"
+}
